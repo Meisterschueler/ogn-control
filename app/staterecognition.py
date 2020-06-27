73bde8c8b91af185e4da3edb4de6547d1b6db66e
Konstantin Gründger <kammermark@gmx.de>
2020-06-27 13:12:09 +0200

    Added landing detection

--- app/staterecognition.py
+++ app/staterecognition.py
@@ -6,17 +6,15 @@ class State(enum.Enum):
 
 
 class StateMachine():
-    aircrafts = {}
-
     def __init__(self, elevation=0, max_last_messages=10):
         self.elevation = elevation
         self.max_last_messages = max_last_messages
+        self.aircrafts = {}
 
     def add_message(self, message):
         address = message['address']
         if address not in self.aircrafts:
-            self.aircrafts[address] = {'state': State.UNKNOWN, 'messages': []}
-            self.aircrafts[address]['messages'].append(message)
+            self.aircrafts[address] = {'state': State.UNKNOWN, 'messages': [message]}
             return
 
         last_state = self.aircrafts[address]['state']
@@ -29,19 +27,27 @@ class StateMachine():
 
         min_airborne_altitude = self.elevation + 200
         min_moving_speed = 10
-        min_starting_speed = 30
+        min_airborne_speed = 30
 
-        if last_state == State.UNKNOWN and last_ground_speed < 10 and last_altitude < min_airborne_altitude:
-            if ground_speed < min_moving_speed and altitude < min_airborne_altitude:
-                state = State.GROUND
-            elif ground_speed < min_starting_speed and altitude < min_airborne_altitude:
-                state = State.MOVING
+        if last_state == State.UNKNOWN:
+            if last_ground_speed < min_moving_speed and last_altitude < min_airborne_altitude:
+                if ground_speed < min_moving_speed and altitude < min_airborne_altitude:
+                    state = State.GROUND
+                elif ground_speed < min_airborne_speed and altitude < min_airborne_altitude:
+                    state = State.MOVING
+                else:
+                    state = State.UNKNOWN
+            elif last_ground_speed >= min_airborne_speed and last_altitude >= min_airborne_altitude:
+                if ground_speed >= min_airborne_speed and altitude >= min_airborne_altitude:
+                    state = State.AIRBORNE
+                else:
+                    state = State.UNKNOWN
             else:
                 state = State.UNKNOWN
         elif last_state == State.GROUND:
             if ground_speed < min_moving_speed and altitude < min_airborne_altitude:
                 state = State.GROUND
-            elif ground_speed < min_starting_speed and altitude < min_airborne_altitude:
+            elif ground_speed < min_airborne_speed and altitude < min_airborne_altitude:
                 state = State.MOVING
             elif altitude < min_airborne_altitude:
                 state = State.STARTING
@@ -50,21 +56,33 @@ class StateMachine():
         elif last_state == State.MOVING:
             if ground_speed < min_moving_speed and altitude < min_airborne_altitude:
                 state = State.GROUND
-            elif ground_speed < min_starting_speed and altitude < min_airborne_altitude:
+            elif ground_speed < min_airborne_speed and altitude < min_airborne_altitude:
                 state = State.MOVING
             elif altitude < min_airborne_altitude:
                 state = State.STARTING
             else:
                 state = State.UNKNOWN
         elif last_state == State.STARTING:
-            if ground_speed >= min_starting_speed and altitude < min_airborne_altitude:
+            if ground_speed >= min_airborne_speed and altitude < min_airborne_altitude:
                 state = State.STARTING
-            elif ground_speed >= min_starting_speed and altitude >= min_airborne_altitude:
+            elif ground_speed >= min_airborne_speed and altitude >= min_airborne_altitude:
                 state = State.AIRBORNE
             else:
                 state = State.UNKNOWN
         elif last_state == State.AIRBORNE:
-            state = State.AIRBORNE
+            if ground_speed >= min_airborne_speed and altitude >= min_airborne_altitude:
+                state = State.AIRBORNE
+            elif ground_speed >= min_airborne_speed and altitude < min_airborne_altitude:
+                state = State.LANDING
+            else:
+                state = State.UNKNOWN
+        elif last_state == State.LANDING:
+            if ground_speed >= min_airborne_speed and altitude < min_airborne_altitude:
+                state = State.LANDING
+            elif ground_speed < min_airborne_speed and ground_speed >= min_moving_speed and altitude < min_airborne_altitude:
+                state = State.MOVING
+            else:
+                state = State.UNKNOWN
         else:
             state = State.UNKNOWN
 
--- tests/test_staterecognition.py
+++ tests/test_staterecognition.py
@@ -25,4 +25,32 @@ class Test(unittest.TestCase):
 
         for message in messages:
             state_machine.add_message(message)
-            self.assertEqual(message['expected_state'], state_machine.get_state(address='DD1234'))
+
+            state = state_machine.get_state(address='DD1234')
+            expected_state = message['expected_state']
+            self.assertEqual(expected_state, state)
+
+    def test_simple_landing(self):
+        messages = [
+            {'expected_state': State.UNKNOWN,  'timestamp': datetime(2020, 6, 27, 10, 30,  0), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 950, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.AIRBORNE, 'timestamp': datetime(2020, 6, 27, 10, 30, 10), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 900, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.AIRBORNE, 'timestamp': datetime(2020, 6, 27, 10, 30, 20), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 850, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.AIRBORNE, 'timestamp': datetime(2020, 6, 27, 10, 30, 30), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 800, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.LANDING,  'timestamp': datetime(2020, 6, 27, 10, 30, 40), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 750, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.LANDING,  'timestamp': datetime(2020, 6, 27, 10, 30, 50), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 700, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.LANDING,  'timestamp': datetime(2020, 6, 27, 10, 31,  0), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 650, 'climb_rate': 5.0, 'track': 90.0},
+            {'expected_state': State.LANDING,  'timestamp': datetime(2020, 6, 27, 10, 31, 10), 'address': 'DD1234', 'ground_speed': 90.0, 'altitude': 600, 'climb_rate': 0.0, 'track': 90.0},
+            {'expected_state': State.LANDING,  'timestamp': datetime(2020, 6, 27, 10, 31, 20), 'address': 'DD1234', 'ground_speed': 50.0, 'altitude': 600, 'climb_rate': 0.0, 'track': 90.0},
+            {'expected_state': State.MOVING,   'timestamp': datetime(2020, 6, 27, 10, 31, 30), 'address': 'DD1234', 'ground_speed': 10.0, 'altitude': 600, 'climb_rate': 0.0, 'track': 90.0},
+            {'expected_state': State.GROUND,   'timestamp': datetime(2020, 6, 27, 10, 31, 40), 'address': 'DD1234', 'ground_speed':  0.0, 'altitude': 600, 'climb_rate': 0.0, 'track': 90.0},
+            {'expected_state': State.GROUND,  ' timestamp': datetime(2020, 6, 27, 10, 31, 50), 'address': 'DD1234', 'ground_speed':  0.0, 'altitude': 600, 'climb_rate': 0.0, 'track': 90.0},
+        ]
+
+        state_machine = StateMachine(elevation=600)
+
+        for message in messages:
+            state_machine.add_message(message)
+
+            state = state_machine.get_state(address='DD1234')
+            expected_state = message['expected_state']
+            self.assertEqual(expected_state, state)
