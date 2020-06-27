5ecab487b9d6ea55bd7f0463bac5e3290246345c
Konstantin Gründger <kammermark@gmx.de>
2020-06-27 09:40:35 +0200

    Add setup.py

---
+++ README.md
@@ -0,0 +1,20 @@
+# ogn-control
+
+[![Build Status](https://travis-ci.org/Meisterschueler/ogn-control.svg?branch=master)](https://travis-ci.org/Meisterschueler/ogn-control)
+[![PyPi Version](https://img.shields.io/pypi/v/ogn-control.svg)](https://pypi.python.org/pypi/ogn-control)
+[![Coverage Status](https://coveralls.io/repos/github/Meisterschueler/ogn-control/badge.svg?branch=master)](https://coveralls.io/github/Meisterschueler/ogn-control?branch=master)
+
+A python3 module for the [Open Glider Network](http://wiki.glidernet.org/).
+It runs on the receiver and visualizes the incoming data
+
+
+## Installation
+
+ogn-control is available at PyPI. So for installation simply use pip:
+
+```
+pip install ogn-control
+```
+
+## License
+Licensed under the [AGPLv3](LICENSE).
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ogn-control',
+    version='0.0.1',
+    description='A python module to control a Open Glider Network (OGN) receiver',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Meisterschueler/ogn-control',
+    author='Konstantin Gründger aka Meisterschueler',
+    author_email='kammermark@gmx.de',
+    license='AGPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+    keywords=['gliding', 'ogn'],
+    packages=['ogn.{}'.format(package) for package in find_packages(where='ogn')],
+    python_requires='>=3',
+    install_requires=[
+        'Flask==1.1.1',
+        'Flask-SocketIO==4.3.0',
+        'eventlet==0.25.1',
+        'Flask-Bootstrap==3.3.7.1',
+        'ogn-client==0.9.7'
+        ],
+    extras_require={
+        'dev': [
+            'nose==1.3.7',
+            'coveralls==2.0.0',
+            'flake8==3.8.2'
+        ]
+    },
+    zip_safe=False
+)
